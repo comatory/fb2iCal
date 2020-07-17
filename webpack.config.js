181c81e49bced011c96f5957ffe5252e04674114
Ondřej Synáček <ondrejsynacek@fastmail.com>
2020-07-17 19:29:18 +0200

    add webpack for front end builds and serve the assets from backend

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const pkg = require('./package.json')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const { GenerateSW } = require('workbox-webpack-plugin')
+
+const destination = path.join(__dirname, 'dist')
+const isDevelopment = Boolean(process.argv[2] && process.argv[2].includes('mode=development'))
+
+module.exports = {
+  entry: path.join(__dirname, 'lib', 'static', 'scripts.js'),
+  watch: isDevelopment,
+  output: {
+    filename: '[name].[hash].js',
+    path: destination,
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: path.join(__dirname, 'lib', 'static', '{*.ico,*.json,*.png,*.css}'), to: destination, flatten: true },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'lib', 'static', 'index.html'),
+      version: pkg.version,
+      inject: 'body',
+    }),
+    new GenerateSW({
+      swDest: 'sw.js',
+      clientsClaim: true,
+      skipWaiting: true,
+    }),
+  ],
+}
