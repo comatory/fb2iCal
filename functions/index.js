a8c5d12a7b8fc37a933c44efc788feb93a9adcc0
Ondřej Synáček <ondrejsynacek@fastmail.com>
2020-11-15 15:13:49 +0100

    fix issues with deployment due to winston logging getting in the way

--- functions/index.js
+++ functions/index.js
@@ -2,19 +2,19 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin')
 
 const { configureApplication } = require('./lib/app')
-const { createAppLogger } = require('../lib/log-utils')
-const {
-  createRouteLogger,
-  createErrorLogger,
-} = require('../lib/middlewares')
+// const { createAppLogger } = require('./lib/log-utils')
+// const {
+//   createRouteLogger,
+//   createErrorLogger,
+// } = require('./lib/middlewares')
 
 admin.initializeApp()
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 
-const appLogger = createAppLogger({ dev: isDevelopment })
-const errorLogger = createErrorLogger({ dev: isDevelopment })
-const routeLogger = isDevelopment ? createRouteLogger({ dev: isDevelopment }) : null
+// const appLogger = createAppLogger({ dev: isDevelopment })
+// const errorLogger = createErrorLogger({ dev: isDevelopment })
+// const routeLogger = isDevelopment ? createRouteLogger({ dev: isDevelopment }) : null
 const corsOptions = isDevelopment ? {
   origin: 'http://localhost:5000',
 } : null
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const fs = require('fs')
 const pkg = require('./package.json')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
@@ -6,10 +7,30 @@ const { GenerateSW } = require('workbox-webpack-plugin')
 
 const destination = path.join(__dirname, 'dist')
 const isDevelopment = Boolean(process.argv[2] && process.argv[2].includes('mode=development'))
+const isFirebaseEnv = process.env.NODE_APP === 'firebase'
+const firebaseConfigFilePath = path.join(__dirname, '.firebaserc')
+const hasFirebaseConfig = fs.existsSync(firebaseConfigFilePath)
+
+if (isFirebaseEnv && hasFirebaseConfig) {
+  console.info('Prepare build for Firebase hosting')
+}
+
+const getFirebaseUrl = () => {
+  const contents = fs.readFileSync(firebaseConfigFilePath)
+  const rawContents = contents.toString()
+  const json = JSON.parse(rawContents)
+  const projectName = json.projects ? json.projects.default : null
+
+  if (isDevelopment) {
+    return `http://localhost:5001/${projectName}/uscentral-1/app`
+  }
+
+  return `${projectName}.web.app/app`
+}
 
 module.exports = {
   entry: path.join(__dirname, 'lib', 'static', 'index.js'),
-  watch: isDevelopment,
+  watch: isDevelopment && !isFirebaseEnv,
   output: {
     filename: '[name].[hash].js',
     path: destination,
@@ -23,6 +44,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'lib', 'static', 'index.html'),
       version: pkg.version,
+      serverURL: (isFirebaseEnv && hasFirebaseConfig) ? getFirebaseUrl() : '',
       inject: 'body',
     }),
     new GenerateSW({
