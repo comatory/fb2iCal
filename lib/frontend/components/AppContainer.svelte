978662b55c60f03077920db4e1ce15bcbbc72adf
Ondřej Synáček <ondrejsynacek@fastmail.com>
2020-12-15 22:42:56 +0100

    feature: use Svelte to render event list

--- lib/frontend/components/AppContainer.svelte
+++ lib/frontend/components/AppContainer.svelte
@@ -1,12 +1,18 @@
 <script>
   import TrackingPanel from './TrackingPanel.svelte'
+  import EventList from './EventList.svelte'
 
-  import { configStore } from '../stores'
+  import { configStore, eventStore } from '../stores'
 
   $: showTrackingPanel = $configStore.track === undefined
+  $: showEventList = $eventStore.length > 0
 </script>
 
 {#if showTrackingPanel}
   <TrackingPanel />
+{/if}
+
+{#if showEventList}
+  <EventList />
 {/if}
 
